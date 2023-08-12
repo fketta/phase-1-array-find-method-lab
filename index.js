1db72af19a3f9ec1b0ac7be40ec531edcd8d92d1
Fella Kettani <fellakettani@gmail.com>
2023-08-12 20:25:27 +0100

    finished assignement

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function superbowlWin(array){
+    const winningEntry = array.find(entry => entry.result === "W");
+    if (winningEntry)
+    {return (winningEntry.year)}
+    else
+    return 
+}
+    
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+console.log(superbowlWin(record));
